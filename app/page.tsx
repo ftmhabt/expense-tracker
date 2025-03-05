999b389240de5b4a8878a562e9526a667eacaf0c
ftmhabt <ftmh.a78@gmail.com>
2025-03-05 19:47:54 +0330

    Add table

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { CreditCard, DollarSign } from "lucide-react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { CreditCard, DollarSign, Pencil } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,7 +20,7 @@ export default function Home() {
             <CardTitle className="text-sm font-medium">
               total expenses
             </CardTitle>
-            <DollarSign className="h-4 w-4 text-muted-foreground" />
+            <DollarSign className="h-4 w-4 text-rose-500" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">45$</div>
@@ -20,13 +29,38 @@ export default function Home() {
         <Card className="w-full bg-amber-200/60">
           <CardHeader className="flex flex-row justify-between items-center space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">total incomes</CardTitle>
-            <CreditCard className="h-4 w-4 text-muted-foreground" />
+            <CreditCard className="h-4 w-4 text-rose-500" />
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">45$</div>
           </CardContent>
         </Card>
       </div>
+      {/*table*/}
+      <Card className="w-full bg-amber-200/60 border-0">
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow className="border-b-rose-500 *:text-rose-500 hover:bg-rose-500/10">
+                <TableHead>Transaction</TableHead>
+                <TableHead>Amount</TableHead>
+                <TableHead>Action</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              <TableRow className="hover:bg-rose-500/10">
+                <TableCell>g</TableCell>
+                <TableCell>g</TableCell>
+                <TableCell>
+                  <Link href={"/edit"}>
+                    <Pencil className="mr-2 h-4 w-4 cursor-pointer hover:scale-105 transition-all ease-in-out" />
+                  </Link>
+                </TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
     </>
   );
 }
