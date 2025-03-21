7b90f360386ac029b38ba08cffc923d0ec9bfc0c
ftmhabt <ftmh.a78@gmail.com>
2025-03-21 21:24:55 +0330

    Fix bugs

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -1,13 +1,17 @@
 import TransactionForm from "@/components/transaction-form";
 import { getTransactionById } from "@/lib/actions";
 
-export default async function page({ params }: { params: { id: string } }) {
-  const transactionId = params.id;
-  const transaction = await getTransactionById(transactionId);
-
-  return transaction?.id == "" ? (
-    <div>no transaction found</div>
-  ) : (
+export default async function page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const transaction = await getTransactionById(id);
+  console.log(transaction);
+  return transaction?.id ? (
     <TransactionForm />
+  ) : (
+    <div>no transaction found</div>
   );
 }
